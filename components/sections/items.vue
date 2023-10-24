c075f97f60c161c884971bbf500d7f8eef3a1c62
latifa-banna <latifabanna94@gmail.com>
2023-10-24 11:23:26 +0100

    commit2

--- components/sections/items.vue
+++ components/sections/items.vue
@@ -2,20 +2,20 @@
     <div>
     <div class="container">
       <div class="box">
-        <div>Ultra-fast Internet</div>
-        <div>150mb/s</div>
+        <div>{{$settings.items.fastInternet.text}}</div>
+        <div>{{$settings.items.fastInternet.number}}mb/s</div>
       </div>
       <div class="box">
-        <div>Available Spaces</div>
-        <div>50+</div>
+        <div>{{$settings.items.spaces.text}}</div>
+        <div>{{$settings.items.spaces.number}}+</div>
       </div>
       <div class="box">
-        <div>Active Members</div>
-        <div>200+</div>
+        <div>{{$settings.items.activeNumber.text}}</div>
+        <div>{{$settings.items.activeNumber.number}}+</div>
       </div>
       <div class="box">
-        <div>Positive Review</div>
-        <div>99.9%</div>
+        <div>{{$settings.items.positiveReview.text}}</div>
+        <div>{{$settings.items.positiveReview.number}}%</div>
       </div>
     </div>
     <div class="container_text_center">
